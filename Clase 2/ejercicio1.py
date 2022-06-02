69e047bca0155b23f33ba70aa390e952ef31a0d4
Claudia Redondo <clarema1716@gmail.com>
2022-06-01 22:57:23 -0500

    Agregado Clase2

---
+++ Clase 2/ejercicio1.py
@@ -0,0 +1,9 @@
+#verificar si la expresión "6/2 (1+2)" da como resultado 1 ó 9
+#imprimir resultado y esplicación de por qué da 1 ó 9
+# Proceso
+
+v1= 6/2
+v2= 1+2
+v3= 6/2 * (1+2)
+print(v3)
+print("El resultado en la otra calculador5a es 1, porque no se respeta la precedencia de operadores")
